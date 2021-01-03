e9d6324eb87d02a890b4c324c942757c01654298
У М <umalat.magma@gmail.com>
2021-01-03 22:31:17 +0300

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import React from 'react'
 import classes from './App.module.css'
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
+import Groups from './pages/Groups/Groups'
+import Students from './pages/Students/Students'
 
 function App() {
   return (
-    
+    <BrowserRouter>
+      <nav className={classes.Nav}>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/groups">Groups</Link>
+        </li>
+        <li>
+          <Link to="/students">Students</Link>
+        </li>
+      </nav>
+      <Switch>
+        <Route path="/groups">
+          <Groups />
+        </Route>
+        <Route path="/students">
+          <Students />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   )
 }
 

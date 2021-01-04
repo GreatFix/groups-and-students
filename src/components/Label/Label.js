9bbb6f10aa7c774bbe5737f9a74d588fcd152bb8
У М <umalat.magma@gmail.com>
2021-01-04 16:56:13 +0300

    added a onClick to "Label"

--- src/components/Label/Label.js
+++ src/components/Label/Label.js
@@ -2,7 +2,11 @@ import React from 'react'
 import classes from './Label.module.css'
 
 const Label = (props) => {
-  return <p className={classes.Label}>{props.children}</p>
+  return (
+    <p className={classes.Label} onClick={props.onClick}>
+      {props.children}
+    </p>
+  )
 }
 
 export default Label

8bc156d847f8e518caa6471cfacbc580a21b478a
У М <umalat.magma@gmail.com>
2021-01-04 16:50:27 +0300

    "Button" written and styled

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,9 +2,30 @@ import React from 'react'
 import classes from './Button.module.css'
 
 const Button = (props) => {
-    return(
-
-    )
+  let color
+  switch (props.color) {
+    case 'danger':
+      color = '#C9003F'
+      break
+    case 'info':
+      color = '#00BABA'
+      break
+    case 'succes':
+      color = '#40C81E'
+      break
+    default:
+      color = '#0048BA'
+  }
+  return (
+    <button
+      disabled={props.disabled}
+      className={classes.Button}
+      onClick={props.onClick}
+      style={{ backgroundColor: color }}
+    >
+      {props.children}
+    </button>
+  )
 }
 
 export default Button

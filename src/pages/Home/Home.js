504209711286658c035e6e981a8f8d6faf9b132c
У М <umalat.magma@gmail.com>
2021-01-08 16:39:46 +0300

    Added diagram in the "Home" page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import classes from './Home.module.css'
-
+import Diagram from '../../images/Diagram.png'
 const Home = (props) => {
-    return ()
+  return (
+    <div className={classes.Home}>
+      <img src={Diagram} alt={'Диаграмма моделей'} />
+    </div>
+  )
 }
 
 export default Home

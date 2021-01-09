0f93cf0f3d9d2a6af840688ae5db86dcbe544966
У М <umalat.magma@gmail.com>
2021-01-09 23:50:44 +0300

    addition: added animation to the page "Home"

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import classes from './Home.module.css'
 import Diagram from '../../images/Diagram.png'
-const Home = (props) => {
+import { useSpring, animated } from 'react-spring'
+
+const calc = (x, y) => [-(y - window.innerHeight / 2) / 20, (x - window.innerWidth / 2) / 20, 1.1]
+const trans = (x, y, s) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
+
+const Home = () => {
+  const [spring, set] = useSpring(() => ({
+    xys: [0, 0, 1],
+    config: { mass: 5, tension: 350, friction: 40 },
+  }))
+
   return (
     <div className={classes.Home}>
-      <img src={Diagram} alt={'Диаграмма моделей'} />
+      <animated.img
+        src={Diagram}
+        alt={'Models diagram'}
+        onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
+        onMouseLeave={() => set({ xys: [0, 0, 1] })}
+        style={{ transform: spring.xys.interpolate(trans) }}
+      />
     </div>
   )
 }
